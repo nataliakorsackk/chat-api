5fdbd1d0f76323a97a6425f38a27530eb9a1e1fa
Natália Korsack <natalia-rosa2@educar.rs.gov.br>
2024-09-02 13:06:56 -0300

    parte três do chat com algumas mudanças e alterações para o funcionamento

--- src/api.js
+++ src/api.js
@@ -1,27 +1,39 @@
-var express = require ("express");
-var app = express();
-
-app.use(express.urlencoded({extend : true}));
+const express = require("express"); 
+const app = express(); 
+app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 
 const router = express.Router();
 
 app.use('/', router.get('/', (req, res)=>{
-    res.status(200).send("<h1>API-CHAT</h1>")
+    res.status(200).send("<h1>API - CHAT</h1>")
+}))
+
+app.use('/', router.get('/sobre',(req, res, next)=>{
+    res.status(200).send({
+        "nome":"API - CHAT",
+        "versão":"0.1.0",
+        "autor":"Tales Rizzii"
+    });
 }));
 
-app.use('/', router.get('/sobre', (req, res)=>{
-    res.status(200).send({
-        "nome": "Api=chat",
-        "versao": "0.1.0",
-        "autor": "Natalia korsack"
-    })
- }));
+app.use("/salas",router.get("/salas", async (req, res,next) => {
+    const token = require("./util/token");
+    const salaController = require("./controllers/salaControllers.js");
+    const test = await token.checkToken(req.headers.token,req.headers.iduser,req.headers.nick);
+    console.log(test)
+    if (test) {
+        let resp = await salaController.get();
+        res.status(200).send(resp);
+    } else {
+        res.status(401).send({msg:"Seu usuário não foi autorizado, verifique."});
+    }
+})); 
 
- app.use("/salas", router.get("/salas", async (req, res, next)=>{
-    const salaController = require("./controllers/salaController");
-    const resp = await salaController.get();
+app.use("/entrar", router.post("/entrar", async(req, res, next) => {
+    const usuarioController = require("./controllers/usuarioController.js");
+    let resp = await usuarioController.entrar(req.body.nick);
     res.status(200).send(resp);
- }));
+}));
 
 module.exports=app;
--- src/models/salaModel.js
+++ src/models/salaModel.js
@@ -1,25 +1,45 @@
-function listarSalas(){
-    return [
-        {
-            /**"_id": {
-            "$oid": "643ece43ea11e6e5b0421f10"
-        },**/
-            "nome": "Guerreiros da infocimol",
-            "tipo": "publica"
-        },{
-            /**"_id": {
-                "$oid": "643ecec1ea11e6e5b0421f12"
-            },**/
-                "nome": "Só os melhores",
-                "tipo": "privada",
-                "chave": "info632b"
-            },{/** 
-                "_id": {
-                "$oid": "643ecec1ea11e6e5b0421f18"
-            },*/
-                "nome": "Fãs do js",
-                "tipo": "publica"
+const db = require("./db");
+
+let listarSalas = async () => {
+    let salas = await db.findAll("salas");
+    console.log("salas: "+salas);
+    return salas;
+};
+
+let buscarSala = async (idsala) => {
+    return db.findOne("salas",idsala);
+};  
+
+let atualizarMensagens = async (sala) => {
+    return await db.updateOne("salas", sala,{_id:sala._id});
+};  
+
+let buscarMensagens = async (idsala, timestamp) => {
+    let sala = await buscarSala(idsala);
+    if (sala.msgs) {
+      let msgs = [];
+      sala.msgs.forEach((msg) => {
+        if (msg.timestamp >= timestamp) {
+          msgs.push(msg);
         }
-    ];
+      });
+      return msgs;
+    }
+    return [];
+    
 }
-    module.exports = {listarSalas};
+
+let sairSala = async (iduser) => {
+  let user = await db.findOne("usuarios", iduser);
+
+  if (user && user.sala) {
+      user.sala = null;  
+      return await db.updateOne("usuarios", user, { _id: user._id });
+  }
+
+  return null;
+};
+
+
+
+module.exports = {listarSalas, buscarSala, atualizarMensagens, buscarMensagens, sairSala};
